24693003692a3f4b4e9e4547896c01bfcf8fe738
Shivam8299 <shivammishra0381@gmail.com>
2024-01-18 21:34:22 +0530

    hello

---
+++ mishra.js
@@ -0,0 +1,14 @@
+const { getRawInput } = require("readline-sync")
+
+s.jfoiea g oiewfjlids
+falseg
+dg
+rae
+WebGL2RenderingContextreg
+RegExpr
+static getDerivedStateFromProps(nextProps, prevState) {
+    static getDerivedStateFromProps(nextProps, prevState) {
+        getRawInputg
+    }
+    
+}
